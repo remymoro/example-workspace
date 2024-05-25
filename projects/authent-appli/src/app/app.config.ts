79c060ec1cfc379bf7078cb300d14026f20c3ed5
moro <moro.remy879@gmail.com>
2024-05-25 14:55:00 +0200

    code: install project, ngrx signal , material

---
+++ projects/authent-appli/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    // provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+  ],
+};
